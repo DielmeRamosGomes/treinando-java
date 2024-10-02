3cbcb4e9cc5f141a365a91c01e9bd6e4c4133ced
Dielme Ramos Gomes <73501790+DielmeRamosGomes@users.noreply.github.com>
2024-10-02 13:10:01 -0300

    atualizando projeto rep local
    arquivos

---
+++ src/main/java/Arquivo/Arquivo.java
@@ -0,0 +1,14 @@
+package Arquivo;
+
+public class Arquivo {
+
+    public static String lerArquivo(String path) {
+        String mensagem = "";
+        
+
+
+        return mensagem;
+    }
+
+
+}
