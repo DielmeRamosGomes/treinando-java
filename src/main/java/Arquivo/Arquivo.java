52133093dc6d87b19c0dee728b43bd3c031d0972
Dielme Ramos Gomes <73501790+DielmeRamosGomes@users.noreply.github.com>
2024-10-02 13:45:12 -0300

    ler arquivo pronto
    método para ler arquivo

--- src/main/java/Arquivo/Arquivo.java
+++ src/main/java/Arquivo/Arquivo.java
@@ -1,13 +1,22 @@
 package Arquivo;
 
+import java.io.*;
+
 public class Arquivo {
 
-    public static String lerArquivo(String path) {
-        String mensagem = "";
-        
-
-
-        return mensagem;
+    public static StringBuilder lerArquivo(String path) {
+        String linha;
+        var msg = new StringBuilder();
+        try {
+            var arquivo = new FileReader(path);
+            var bufferedReader = new BufferedReader(arquivo);
+            while ((linha = bufferedReader.readLine()) != null) {
+                msg.append(linha).append("\n");
+            }
+        } catch (NullPointerException | IOException e) {
+            System.out.println("Erro linha nula ou IOException");
+        }
+        return msg;
     }
 
 
--- src/main/java/Arquivo/Main.java
+++ src/main/java/Arquivo/Main.java
@@ -2,6 +2,7 @@ package Arquivo;
 
 public class Main {
     public static void main(String[] args) {
-
+        String path = "D:\\Programas no Visual Studio Code\\Java\\treinando-java\\src\\main\\java\\Arquivo\\teste.txt";
+        System.out.println(Arquivo.lerArquivo(path));
     }
 }
